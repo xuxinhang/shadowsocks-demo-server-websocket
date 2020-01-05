7f758d375c7edc0e22583e694fa2038534b7d993
Xu Xinhang <xuxinhang4567#126.com>
2020-01-05 17:38:02 +0800

    完成

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const net = require('net');
 const crypto = require('crypto');
 
+// For Debug
+let remoteSocketOpenCount = 0;
+
 function EVP_BytesToKey (password, key_len, iv_len) {
   var count, d, data, i, iv, key, m, md5, ms;
   m = [];
@@ -26,135 +29,153 @@ function EVP_BytesToKey (password, key_len, iv_len) {
 };
 
 
-
-
-// Prepare Crypto
-
-class Decoder {
-  constructor (password, IV) {
-    const [myKey, myIV_] = EVP_BytesToKey(Buffer.from('password', 'binary'), 16, 16);
-    const myIV = IV;
-    const decipher = crypto.createDecipheriv('aes-128-ctr', myKey, myIV);
-    this.decipher = decipher;
-    let result = Buffer.from([]);
-    this.result = result;
-    decipher.on('readable', () => {
-      let chunk;
-      while(chunk = decipher.read()) {
-        result = Buffer.concat([result, chunk]);
-      }
-    });
-    decipher.on('end', () => {
-      console.log(result.toString());
-    });
+class DataBuffer {
+  constructor() {
+    this.slices = [];
   }
-  write (data) {
-    this.decipher.write(data);
+  push(buf) {
+    return this.slices.push(buf);
   }
-  end () {
-    this.decipher.end();
+  shift() {
+    return this.slices.shift();
+  }
+  get length () {
+    return this.slices.length;
   }
 }
-
-// remote connection poll
-const remoteSocketPool = {};
 
 
 const server = new net.Server({});
 
-server.on('connection', function connected (socket) {
+server.on('connection', function connected (local) {
   console.log('a connection coming');
   let vi_sent = false;
   let myIV = null;
-  let decipher, cipher;
+  let decipher = null, cipher = null;
   const [myKey, myIV_] = EVP_BytesToKey(Buffer.from('password', 'binary'), 16, 16);
 
   let remote = null; // the socket to the remote server.
+  let remoteConnected = false;
 
-  socket.on('connect', () => {
-    console.log('local connect');
+  let localConHeader = null;
+  const localDataBuffer = new DataBuffer();
+
+  const clean = () => {
+    remote = null;
+    connection = null;
+    cipher = null;
+    decipher = null;
+    console.log('local connection cleaned.');
+  };
+
+  local.on('connect', () => {
+    console.log('local on connect');
   });
 
-  socket.on('data', (data) => {
-    console.log('socket data now.');
+  local.on('data', (raw) => {
+    console.log('local on data.');
 
-    if (!myIV) {
-      myIV = data.slice(0, 16);
-      data = data.slice(16);
-      decipher = crypto.createDecipheriv('aes-128-ctr', myKey, myIV);
-      cipher = crypto.createCipheriv('aes-128-ctr', myKey, myIV);
-    }
+    if (remote && remoteConnected) {
+      const data = decipher.update(raw);
+      remote.write(data);
 
-    let result = decipher.update(data);
+    } else if (remote && !remoteConnected) {
+      const data = decipher.update(raw);
+      // localDataBuffer.push(data);
+      remote.write(data);
 
-    const parsed = parseData(result);
-    if (!parsed) {
-      socket.end();
-      return;
-    }
+    } else {
+      let rawData;
+      if (!myIV) {
+        myIV = raw.slice(0, 16);
+        rawData = raw.slice(16);
+        decipher = crypto.createDecipheriv('aes-128-ctr', myKey, myIV);
+        cipher = crypto.createCipheriv('aes-128-ctr', myKey, myIV);
+      }
 
-    const { type: remoteAddrType, host: remoteHost, port: remotePort, payload } = parsed;
-    console.log(remoteHost, remotePort);
+      let data = decipher.update(rawData);
 
-    socket.pause();
+      if (!localConHeader) {
+        const parsed = parseHeader(data);
+        if (!parsed) {
+          local.end();
+          return;
+        }
+        localConHeader = parsed;
+      }
 
-    // Open connection to remote
-    const poolKey = `${remoteHost}:${remotePort}`;
-    if (true || !(poolKey in remoteSocketPool)) {
-      const remote = net.connect({
+      const { type: remoteAddrType, host: remoteHost, port: remotePort, headerLength } = localConHeader;
+      const poolKey = `${remoteHost}:${remotePort}`;
+      console.log(poolKey);
+      const payload = data.slice(headerLength);
+
+      // socket.pause();
+
+      // Save data into buffers
+      // localDataBuffer.push(payload);
+
+      // Open connection to remote
+      console.log(`create new connection to remote server, accu=${++remoteSocketOpenCount}`);
+      remote = remote || net.connect({
         host: remoteHost,
         port: remotePort,
       });
-      remoteSocketPool[poolKey] = remote;
+
+      remote.write(payload);
+
       remote.on('connect', () => {
         console.log('Connected to remote port');
-        socket.resume();
+        // socket.resume();
+        remoteConnected = true;
+
+        // Write saved data in buffer
+        // while (localDataBuffer.length) {
+        //   remote.write(localDataBuffer.shift());
+        // }
+      });
+
+      remote.on('data', (buf) => {
+        const payload = cipher.update(buf);
+        let data = payload;
+        if (!vi_sent) {
+          data = Buffer.concat([myIV, payload]);
+          vi_sent = true;
+        }
+        if (!local.write(data)) {
+          // remote.pause();
+        }
+      });
+
+      remote.on('drain', () => {
+        // socket.resume();
+      });
+
+      remote.on('end', () => {
+        console.info('remote connection on end');
+        local.end();
+      })
+
+      remote.on('error', (e) => {
+        console.error(e);
+      });
+
+      remote.on('close', (hadError) => {
+        console.info('remote connection on close');
+        if (hadError) {
+          local.end();
+        } else {
+          local.destroy();
+        }
       });
     }
-
-    remote = remoteSocketPool[poolKey];
-
-    remote.write(payload, () => {
-      console.log('payload written successfully');
-    });
-
-    remote.on('data', (buf) => {
-      const payload = cipher.update(buf);
-      let data = payload;
-      if (vi_sent === false) {
-        data = Buffer.concat([myIV, payload]);
-        vi_sent = true;
-      }
-      if (!socket.write(data)) {
-        // remote.pause();
-      }
-    });
-
-    remote.on('end', () => {
-      console.info('remote socket on end');
-      socket.end();
-    })
-
-    remote.on('error', (e) => {
-      console.error(e);
-    });
-
-    remote.on('close', (hadError) => {
-      console.info('remote socket on close');
-      if (hadError) {
-        socket.end();
-      } else {
-        socket.destroy();
-      }
-    })
   });
 
-  socket.on('end', () => {
+  local.on('end', () => {
     console.log('local connection end');
     remote.end();
   });
 
-  socket.on('close', hadError => {
+  local.on('close', hadError => {
     if (hadError) {
       remote.destroy();
     } else {
@@ -162,32 +183,36 @@ server.on('connection', function connected (socket) {
     }
   });
 
-  socket.on('error', e => {
+  local.on('error', e => {
     console.error(e);
+  });
+
+  local.on('drain', () => {
+    // remote.resume();
   });
 });
 
 server.listen(8388);
 
-function parseData (data) {
+function parseHeader (data) {
   const addrType = Number(data[0]);
   let host, port, payload;
   switch (addrType) {
     case 0x01:
       host = data.slice(1, 5);
       port = data.readUInt16BE(5); // data.slice(5, 7);
-      payload = data.slice(7);
+      headerLength = 7;
       break;
     case 0x03:
       const len = Number(data[1]);
       host = data.slice(2, 2 + len);
       port = data.readUInt16BE(2 + len); // data.slice(2 + len, 2 + len + 2);
-      payload = data.slice(2 + len + 2);
+      headerLength = 2 + len + 2;
       break;
     case 0x04:
       host = data.slice(1, 17);
       port = data.readUInt16BE(17); // data.slice(17, 19);
-      payload = data.slice(19);
+      headerLength = 19;
       break;
     default:
       console.log(`Unknow Address Type == ${addrType}`);
@@ -199,7 +224,7 @@ function parseData (data) {
     type: addrType,
     host: String(host),
     port: Number(port),
-    payload,
+    headerLength,
   };
 }
 
